861b5097b6382f9be0c82d3396326593a27d219d
CAccampo <carmenaccampo@gmail.com>
2025-09-13 22:37:26 -0500

    added validation to other Action options

--- src/core/Actions.js
+++ src/core/Actions.js
@@ -7,20 +7,27 @@ const Actions = {
       case "1": {
         const cIdx = parseInt(await ui.ask("Choose card index from hand: "), 10);
         const sIdx = parseInt(await ui.ask("Choose caravan index (0-2): "), 10);
+        
         if (!Validator.canPlaceOnOwn(player, cIdx, sIdx, phase="main")) return false;
         return Placement.placeOnOwn(player, cIdx, sIdx); // returns true/false
       }
       case "2": {
         const cIdx = parseInt(await ui.ask("Choose card index from hand: "), 10);
         const sIdx = parseInt(await ui.ask("Choose opponent caravan index (0-2): "), 10);
+        
+        if (!Validator.canPlaceOnOpponent(player, cIdx, opponent, sIdx)) return false;
         return Placement.placeOnOpponent(player, cIdx, opponent, sIdx);
       }
       case "3": {
         const sIdx = parseInt(await ui.ask("Choose caravan index (0-2): "), 10);
+        
+        if (!Validator.canDiscardCaravan(player, sIdx)) return false;
         return Placement.discardCaravan(player, sIdx);
       }
       case "4": {
         const cIdx = parseInt(await ui.ask("Choose card index from hand: "), 10);
+
+        if (!Validator.canDiscardHandCard(player, cIdx));
         return Placement.discardHandCard(player, cIdx);
       }
       default:
